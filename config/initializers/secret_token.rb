78be759406a59592c0de2a19870719b84be8e9e2
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-05-21 00:12:53 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularjsNfl::Application.config.secret_key_base = '7375fb61ed89ac7a60502dc32dcc33e88d6f70eabfa0db31a0684f4180c8df763a37faa2c44972feb0d1b3510a2b9504357e4a6b5ccf9eb321c6c83c59d6ccd7'
